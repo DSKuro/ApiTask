bbfbefb95d5a544a994905bec1f834523807a77f
ALEX-PC\alexd <alex.dutikovsamp@yandex.ru>
2025-06-03 16:30:20 +0300

    Сделал MessageBox: 1 вариант с MessageBox.Avalonia, 2 - собственный (не очень хороший)

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,4 +1,9 @@
+using ApiTask.ViewModels;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Enums;
+using System.Threading.Tasks;
 
 namespace ApiTask.Views
 {
@@ -7,6 +12,17 @@ namespace ApiTask.Views
         public MainWindow()
         {
             InitializeComponent();
+            //connectToApi();
+        }
+
+        public void ClickHandler(object sender, RoutedEventArgs args)
+        {
+            //MessageBox.Show(this, "test", "Title", MessageBox.MessageBoxButtons.Ok);
+                var box = MessageBoxManager
+               .GetMessageBoxStandard("Caption", "test",
+                   ButtonEnum.YesNo);
+
+                var result =  box.ShowWindowDialogAsync(this);
         }
     }
 }
