f1e9d62a75fa3d5a91f4983fd20d1759905771e1
ALEX-PC\alexd <alex.dutikovsamp@yandex.ru>
2025-06-03 13:42:54 +0300

    Занёс api-ключ в user-secrets

---
+++ ViewModels/ReadConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiTask.ViewModels
+{
+    public static class ReadConfiguration
+    {
+        private static readonly IConfiguration configuration = new ConfigurationBuilder()
+            .AddUserSecrets<Program>()
+            .Build();
+
+        public static String getValueByKey(String key)
+        {
+            if (configuration[key] == null)
+            {
+                return "";
+            }
+
+            return configuration[key];
+        }
+    }
+}
